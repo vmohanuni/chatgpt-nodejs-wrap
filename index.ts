c4c523f27e09570fc1789e7b3c9bd87defdcf2ff
Vasanth Mohan <vasanthmohan@VasanthMohanMBP.attlocal.net>
2023-01-05 14:31:50 -0800

    ChatGPT Wrapper

---
+++ index.ts
@@ -0,0 +1,45 @@
+require('dotenv').config();
+import { ChatGPTAPIBrowser } from 'chatgpt'
+const express = require('express')
+const app = express()
+const port = 3000
+
+// use puppeteer to bypass cloudflare (headful because of captchas)
+const api = new ChatGPTAPIBrowser({
+    email: process.env.OPENAI_EMAIL,
+    password: process.env.OPENAI_PASSWORD,
+    isGoogleLogin: true
+})
+
+var lastResult = null;
+
+app.get('/init', async (req, res) => {
+  await api.initSession();
+  res.send("Initalized");
+})
+
+app.get('/prompt', async (req, res) => {
+  var result = null;
+  if (lastResult) {
+      result = await api.sendMessage( req.query.prompt ,  {
+        conversationId: lastResult.conversationId,
+        parentMessageId: lastResult.messageId
+    });
+  } else {
+      result = await api.sendMessage( req.query.prompt );
+  }
+
+  lastResult = result;
+  res.send(result.response)
+})
+
+app.get('/close', async (req, res) => {
+  await api.closeSession();
+  res.send("Closed");
+})
+
+
+app.listen(port, () => {
+  console.log(`Chat GPT listening on ${port}`)
+})
+
